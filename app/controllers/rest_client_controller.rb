adc9e01d1d8487e56245a05491b5c9740222968d
Hardik Joshi <hardy12@gmail.com>
2013-11-26 19:20:11 +0530

    hard coded api url

--- app/controllers/rest_client_controller.rb
+++ app/controllers/rest_client_controller.rb
@@ -5,8 +5,9 @@ class RestClientController < ApplicationController
   end
 
   def create
-    req = RestClient.send(params[:req_method].to_sym, params[:req_url], {'Client-UUID' => params[:client_uuid]}) if ['get', 'delete'].include?(params[:req_method])
-    req = RestClient.send(params[:req_method].to_sym, params[:req_url], eval(params[:req_body]), {'Client-UUID' => params[:client_uuid]}) if ['post', 'put'].include?(params[:req_method])
+    api_end_point = 'https://gtw.iwsinc.com/client_api/v1/' + params[:req_url]
+    req = RestClient.send(params[:req_method].to_sym, api_end_point, {'Client-UUID' => params[:client_uuid]}) if ['get', 'delete'].include?(params[:req_method])
+    req = RestClient.send(params[:req_method].to_sym, api_end_point, eval(params[:req_body]), {'Client-UUID' => params[:client_uuid]}) if ['post', 'put'].include?(params[:req_method])
     @resp = req
     render :action => :new
   end
