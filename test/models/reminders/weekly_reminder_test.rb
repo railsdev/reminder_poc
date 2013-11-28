db397613729d1fdf644c285fb7c5b26d64aeba2f
Hardik Joshi <hardy12@gmail.com>
2013-11-28 18:11:46 +0530

    added check of time difference between two consequtive reminders

--- test/models/reminders/weekly_reminder_test.rb
+++ test/models/reminders/weekly_reminder_test.rb
@@ -21,6 +21,17 @@ class WeeklyReminderTest < ActiveSupport::TestCase
       assert_empty ScheduledReminder.where(reminder_id: reminder_id).to_a
     end
 
+    # TODO: Bad way.. need to refactor
+    def test_day_of_scheduled_reminders
+      @reminder.scheduled_reminders.each{|x| assert_equal x.run_at.wday, @reminder.fq_day }
+    end
+
+    # TODO: Bad way.. need to refactor
+    def test_time_of_schedule_reminders
+      @reminder.scheduled_reminders.each{|x| assert_equal x.run_at.hour, @reminder.fq_time.hour}
+      @reminder.scheduled_reminders.each{|x| assert_equal x.run_at.min, @reminder.fq_time.min}
+    end
+
     def test_different_of_time_between_scheduled_reminders
       faulty_scheduled_reminders = @reminder.scheduled_reminders.to_a.each_cons(2).select{|a, b| (b.run_at - a.run_at).to_i.seconds != 1.week.seconds }
       faulty_scheduled_reminders.must_be_empty
